247e754d28d609411db520221a7efd623448da89
Erick Vera <erickverac90@gmail.com>
2024-07-02 23:26:15 -0600

    Agregado funciones para crear posts

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-import { addTag } from "./tags.js";
+import { addTag, publishPost } from "./tags.js";
+import { createPost, getAllPosts, updatePost, deletePost } from "./postsApi.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const tagsInput = document.getElementById("tagsInput");
   const tagsContainer = document.getElementById("tagsContainer");
+  const addCoverImageButton = document.getElementById("addCoverImageButton");
+  const coverImageUrl = document.getElementById("coverImageUrl");
 
   tagsInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
@@ -10,5 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
       addTag(tagsInput.value, tagsContainer);
       tagsInput.value = "";
     }
+  });
+
+  addCoverImageButton.addEventListener("click", () => {
+    coverImageUrl.style.display =
+      coverImageUrl.style.display === "none" ? "block" : "none";
   });
 });
